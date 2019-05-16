2b194931640b3baee10991beaa263a8411864b6b
marcuspereiradev <vinnycoast@gmail.com>
2019-05-16 15:28:35 -0300

    Configuring babel to use JSX

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import Title from './app'
+'use strict'
 
 import React from 'react'
 import { render } from 'react-dom'
+import Title from './app'
 
 render(
-  React.createElement(Title),
+  <Title />,
   document.querySelector('[data-js="app"]')
 )
